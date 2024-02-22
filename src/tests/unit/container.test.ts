a768c3ece7dc1a5f991b5fb3c835d15da1f3fb01
Success K <successxodev@gmail.com>
2024-02-21 21:06:11 -0500

    Feature/car 24 fix any bugs or issues found during testing (#10)
    * fixing CAR-24
    
    * fixing CAR-24
    
    * fixing CAR-24
    
    * fixing CAR-24
    
    * fixing CAR-24: test endpoint
    
    * fixing CAR-24: test endpoint
    
    * fixing CAR-24: test endpoint
    
    * fixing CAR-24: test timeout
    
    * fixing CAR-24: test timeout

--- src/tests/unit/container.test.ts
+++ src/tests/unit/container.test.ts
@@ -33,7 +33,7 @@ describe("POST /pong", () => {
       "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]",
     );
     expect(response.body.error).toBeNull();
-  }, 10000);
+  }, 15000);
 
   it("returns an error for unsupported languages", async () => {
     const payload = {
